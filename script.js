ab6a1e069c4e4df2d3c7167c0abac4c873cddd54
sofie <aykiademau@gmail.com>
2024-01-01 01:38:20 +0200

    shortest js code with parameters

--- script.js
+++ script.js
@@ -1,97 +1,23 @@
-function addTask_mon(){
-    const val = document.querySelector(".input-in-mon").value;
-    if (val === ""){
-      alert("Try write sth")
-    }
-    else{
-      const task = document.querySelector("#mon-task");
-      const li = document.createElement("li");
-      li.innerHTML = val;
-      task.appendChild(li)
-    }
-    document.querySelector(".input-in-mon").value = "";
-}
+function addTask(day) {
+  const inputSelector = `.input_in_${day}`;
+  const taskSelector = `#${day}-task`;
 
-function addTask_tue(){
-    const val = document.querySelector(".input-in-tue").value;
-    if (val === ""){
-      alert("Try write sth")
-    }
-    else{
-      const task = document.querySelector("#tue-task");
-      const li = document.createElement("li");
-      li.innerHTML = val;
-      task.appendChild(li)
-    }
-    document.querySelector(".input-in-tue").value = "";
-}
+  const inputElement = document.querySelector(inputSelector);
+  
+  if (inputElement) {
+    const val = inputElement.value;
 
-function addTask_wed(){
-    const val = document.querySelector(".input-in-wed").value;
-    if (val === ""){
-      alert("Try write sth")
-    }
-    else{
-      const task = document.querySelector("#wed-task");
+    if (val === "") {
+      alert("Try write sth");
+    } else {
+      const task = document.querySelector(taskSelector);
       const li = document.createElement("li");
       li.innerHTML = val;
-      task.appendChild(li)
+      task.appendChild(li);
     }
-    document.querySelector(".input-in-wed").value = "";
-}
 
-function addTask_thu(){
-    const val = document.querySelector(".input-in-thu").value;
-    if (val === ""){
-      alert("Try write sth")
-    }
-    else{
-      const task = document.querySelector("#thu-task");
-      const li = document.createElement("li");
-      li.innerHTML = val;
-      task.appendChild(li)
-    }
-    document.querySelector(".input-in-thu").value = "";
-}
-
-function addTask_fri(){
-    const val = document.querySelector(".input-in-fri").value;
-    if (val === ""){
-      alert("Try write sth")
-    }
-    else{
-      const task = document.querySelector("#fri-task");
-      const li = document.createElement("li");
-      li.innerHTML = val;
-      task.appendChild(li)
-    }
-    document.querySelector(".input-in-fri").value = "";
-}
-
-function addTask_sat(){
-    const val = document.querySelector(".input-in-sat").value;
-    if (val === ""){
-      alert("Try write sth")
-    }
-    else{
-      const task = document.querySelector("#sat-task");
-      const li = document.createElement("li");
-      li.innerHTML = val;
-      task.appendChild(li)
-    }
-    document.querySelector(".input-in-sat").value = "";
-}
-
-function addTask_sun(){
-    const val = document.querySelector(".input-in-sun").value;
-    if (val === ""){
-      alert("Try write sth")
-    }
-    else{
-      const task = document.querySelector("#sun-task");
-      const li = document.createElement("li");
-      li.innerHTML = val;
-      task.appendChild(li)
-    }
-    document.querySelector(".input-in-sun").value = "";
+    inputElement.value = "";
+  } else {
+    console.error(`Element with selector ${inputSelector} not found.`);
+  }
 }
